4afd8d1a96dc24dcbc6d835eaaf0fc4ce7a9f2a0
Marko Živanović <zivanovic.marko@icloud.com>
2024-12-28 22:04:02 +0100

    initial commit

---
+++ ViewsAndModifiers/WhyDoesSwiftUIUseStructsForViews.swift
@@ -0,0 +1,18 @@
+//
+//  WhyDoesSwiftUIUseStructsForViews.swift
+//  ViewsAndModifiers
+//
+//  Created by Marko Zivanovic on 28.12.24..
+//
+
+import SwiftUI
+
+struct WhyDoesSwiftUIUseStructsForViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WhyDoesSwiftUIUseStructsForViews()
+}
