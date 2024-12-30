c7e65207dadbf1e6f32a5adb497821327091aaa8
Marko Živanović <zivanovic.marko@icloud.com>
2024-12-30 22:52:09 +0100

    Why does SwiftUI use some View for its view type

---
+++ ViewsAndModifiers/WhySwiftUIUseSomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WhySwiftUIUseSomeView.swift
+//  ViewsAndModifiers
+//
+//  Created by Marko Zivanovic on 30.12.24..
+//
+
+import SwiftUI
+
+struct WhySwiftUIUseSomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WhySwiftUIUseSomeView()
+}
